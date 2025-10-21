c945e395f44c1f17f18e0163b36f8bf40c606c99
Tropax2 <afonsolopes031@gmail.com>
2025-10-21 19:28:03 +0100

    All prompts are now part of just one function.

--- main.py
+++ main.py
@@ -5,64 +5,19 @@ from collections import Counter
 
 my_cars = []
 
-print("Hello, this program will store the information about your cars.")
-
-# This is the main function.
-def main():
-    while True:
-        brand = input("What is the car brand?\n")
-        model = input("What is the model of the car?\n")
-        year = input("From which year is the car?\n")
-
-        write_on_data_base(brand, model, year)
-        
-        answer = add_more_cars()
-
-        if answer == 'yes':
-            continue 
-
-        elif answer == 'no':
-            break
-    
-    see_car_brand_and_number = see_brands_and_numbers()
-
-    if see_car_brand_and_number == 'yes':
-        brands_and_numbers()
-
-    elif see_car_brand_and_number == 'no':
-        pass
-
 # Adds the data of the cars to a dictionary.
 def write_on_data_base(brand, model, year):
     my_car = Car(brand.title(), model.title(), year)
     my_cars.append(my_car.structure())
 
-# Checks if the user wants to add more cars.     
-def add_more_cars():
+# Prompts the user.
+def prompt_the_user(prompt):
     while True:
-        add_more_cars = input("Do you want to add more cars? (yes/no)\n")
+        answer = input(f"{prompt} (yes/no)\n")
 
-        if add_more_cars == 'yes':
-            return add_more_cars 
-
-        elif add_more_cars == 'no':
-            return add_more_cars
-
-        else:
-            print("Your answer must be 'yes' or 'no'.")
-            continue
-
-# Checks if the user wants to see the different brands and the number of cars of each brand he owns.
-def see_brands_and_numbers():
-    while True:
-        brand_and_number = input("Do you want to see which brands do you have and how many cars of each brand you own? (yes/no)\n") 
-
-        if brand_and_number == 'yes':
-            return brand_and_number 
-
-        elif brand_and_number == 'no':
-            return brand_and_number 
-        
+        if answer == 'yes' or answer == 'no':
+            return answer 
+               
         else:
             print("Your answer must be 'yes' or 'no'.")
             continue
@@ -71,9 +26,41 @@ def see_brands_and_numbers():
 def brands_and_numbers():
     brands = [brand.get('brand', None) for brand in my_cars]
     print(dict(Counter(brands))) 
-    
 
-main()
+# This is the main function.
+def main():
+    print("Hello, this program will store the information about your cars.")
+
+    while True:
+        brand = input("What is the car brand?\n")
+        model = input("What is the model of the car?\n")
+        year = int(input("From which year is the car?\n"))
+
+        write_on_data_base(brand, model, year)
+        
+        add_more_cars = prompt_the_user("Do you want to add more cars to the list?")
+
+        if add_more_cars == 'yes':
+            continue 
+
+        elif add_more_cars == 'no':
+            break
+    
+    see_car_brand_and_number = prompt_the_user("Do you want to see which brands do you have and how many cars of each brand you own?")
+
+    if see_car_brand_and_number == 'yes':
+        brands_and_numbers()
+
+    elif see_car_brand_and_number == 'no':
+        pass
+
+    final_results = prompt_the_user("Do you want to visualise all the data?")
+
+    if final_results == 'yes':
+        print(my_cars)
+    
+if __name__ == "__main__":
+    main()
 
 
 
